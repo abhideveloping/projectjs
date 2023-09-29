7680dde0e1fd36c7e2f0e51be442be692d7dcc05
abhideveloping <abhinandantiwari150@gmail.com>
2023-09-30 03:40:23 +0530

    i changed this thing again

--- multiple/home.js
+++ multiple/home.js
@@ -1,17 +1,17 @@
-var elem = document.querySelectorAll(".elem");
+var elem = document.querySelectorAll(".elem")
+
 
 elem.forEach(function(val){
-
-    
+    // console.log(val.childNodes[2]);
     val.addEventListener("mouseenter", function(){
-        val.childNodes[3].style.opacity = 1;
-
+        val.childNodes[2].style.opacity = 1
+    });
     val.addEventListener("mouseleave", function(){
-        val.childNodes[3].style.opacity = 0;
+        val.childNodes[2].style.opacity = 0
     });
     val.addEventListener("mousemove", function(dets){
-        val.childNodes[3].style.left = dets.x + "px"
-        val.childNodes[3].style.top = dets.y + "px"
-    })
+        val.childNodes[2].style.left = dets.x + "px"
+        val.childNodes[2].style.top = dets.y + "px"
+    });
 });
-});
+
